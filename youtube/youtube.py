976028317a8d072638b24b8f474aaec06154a74f
xixiHou <hoululin@gmail.com>
2019-12-30 20:22:29 +0800

    优化

--- youtube/youtube.py
+++ youtube/youtube.py
@@ -2,12 +2,12 @@
 from bilibiliupload import *
 import os
 import time
+import glob
 
 video_dir = '/Users/happyhou/onedrive/OneDrive - vlity.ac.id/11'
-# video_dir = '/Users/happyhou/develop/11'
 b = Bilibili()
 
-login_status = b.login('18253940579', '39$XG0G^3s')
+login_status = b.login('username', 'pass')
 print("Login:", login_status)
 
 
@@ -22,11 +22,15 @@ def log(str):
     print("logE>>>> ", str)
 
 
+file_total_count = len(glob.glob1(video_dir, "*.mp4"))
+current = 1
 files = os.listdir(video_dir)
 for file in files:
     pattern = file.split(".", 1)
     if pattern[1] == "mp4":
         log("begin" + pattern[0])
+        print(current, "/", file_total_count)
+        current = current + 1
         upload(os.path.join(video_dir, file), pattern[0],
                os.path.join(video_dir, pattern[0] + ".jpg"))
         log("remove")
@@ -35,9 +39,3 @@ for file in files:
         log("sleep 1 minutes")
         time.sleep(60)
 
-# videoPart = VideoPart('153.mp4', 'this is 153 desc')
-#
-# tags = {u"123", u"Shroud", u"Beaulo"}
-#
-# b.upload(parts=videoPart, title='153 title', tid=65, tag=tags, desc='Uploaded from 23333',
-#          source='http://www.youtube.com', cover=b.cover_up('153.jpg'))

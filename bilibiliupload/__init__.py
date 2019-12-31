947580c839738b9255937ddafdb2f837be12f7eb
xixiHou <hoululin@gmail.com>
2019-12-31 13:56:10 +0800

    自动化全部channel

--- bilibiliupload/__init__.py
+++ bilibiliupload/__init__.py
@@ -1,3 +1,4 @@
 from .bilibili import *
 
 __version__ = '0.2.1'
+print("ssssssssssssssss")
--- youtube/youtube.py
+++ youtube/youtube.py
@@ -1,13 +1,18 @@
 # -*- coding:utf-8 -*-
-from bilibiliupload import *
 import os
 import time
 import glob
-
+import sys
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+bilibiliPath = os.path.split(curPath)[0]+"/bilibiliupload"
+sys.path.append(rootPath)
+sys.path.append(bilibiliPath)
+from bilibiliupload import *
 video_dir = '/Users/happyhou/onedrive/OneDrive - vlity.ac.id/11'
 b = Bilibili()
 
-login_status = b.login('username', 'pass')
+login_status = b.login('18253940579', '39$XG0G^3s')
 print("Login:", login_status)
 
 
@@ -22,20 +27,27 @@ def log(str):
     print("logE>>>> ", str)
 
 
-file_total_count = len(glob.glob1(video_dir, "*.mp4"))
-current = 1
-files = os.listdir(video_dir)
-for file in files:
-    pattern = file.split(".", 1)
-    if pattern[1] == "mp4":
-        log("begin" + pattern[0])
-        print(current, "/", file_total_count)
-        current = current + 1
-        upload(os.path.join(video_dir, file), pattern[0],
-               os.path.join(video_dir, pattern[0] + ".jpg"))
-        log("remove")
-        os.remove(os.path.join(video_dir, file))
-        os.remove(os.path.join(video_dir, pattern[0] + ".jpg"))
-        log("sleep 1 minutes")
-        time.sleep(60)
+def uploadfile():
+    file_total_count = len(glob.glob1(video_dir, "*.mp4"))
+    current = 1
+    files = os.listdir(video_dir)
+    for file in files:
+        pattern = file.split(".", 1)
+        if pattern[1] == "mp4":
+            log("begin" + pattern[0])
+            print(current, "/", file_total_count)
+            current = current + 1
+            upload(os.path.join(video_dir, file), pattern[0],
+                   os.path.join(video_dir, pattern[0] + ".jpg"))
+            log("remove")
+            os.remove(os.path.join(video_dir, file))
+            os.remove(os.path.join(video_dir, pattern[0] + ".jpg"))
+            log("sleep 1 minutes")
+            time.sleep(60)
 
+
+for i in range(221, 435, 70):
+    youtube_dl_commond = "youtube-dl -f best -ciw -o \"%(title)s.%(ext)s\" -v --write-thumbnail --playlist-start {start} --playlist-end {end} https://www.youtube.com/channel/UCunxpZtwW2TN2dk0uoXuNtw"
+    youtube_dl_commond = youtube_dl_commond.format(start=i, end=i + 70)
+    os.system(youtube_dl_commond)
+    uploadfile()
